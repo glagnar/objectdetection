779839463d25285b927328e7eb753681cd9e2fd4
Thomas Gilbert <thomas.gilbert@alexandra.dk>
2020-05-19 14:55:58 +0200

    Add initial python demo

---
+++ python-test/main.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import cv2
+import numpy
+
+def main():
+    print('Program Starting')
+    index = 0
+    arr = []
+    while True:
+        cap = cv2.VideoCapture(index)
+        if not cap.read()[0]:
+            break
+        else:
+            arr.append(index)
+        cap.release()
+        index += 1
+    print(arr)
+
+if __name__ == '__main__':
+    main()
