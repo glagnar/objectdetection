0e35cc741e669def4fdd0c506a45711009bf9ca9
Thomas Gilbert <thomas.gilbert@alexandra.dk>
2020-05-19 22:57:54 +0200

    Downgrade to OpenCV 4.2.0 as it works on macOS, Ubuntu and Raspberry

--- python-test/main.py
+++ python-test/main.py
@@ -7,7 +7,7 @@ def main():
     index = 0
     arr = []
     while True:
-        cap = cv2.VideoCapture(index, cv2.CAP_V4L)
+        cap = cv2.VideoCapture(index)
         if not cap.read()[0]:
             break
         else:
