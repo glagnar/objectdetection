946a23ec35459c95b65e6add85785b76dec4c32c
Thomas Gilbert <thomas.gilbert@alexandra.dk>
2020-05-20 14:39:32 +0200

    Add tpu stuff and requirements

---
+++ python-test/tpu.py
@@ -0,0 +1,13 @@
+import argparse
+
+from edgetpu.detection.engine import DetectionEngine
+
+import time 
+from collections import deque
+
+def main():
+    print('Done')
+
+    
+if __name__ == '__main__':
+    main()
